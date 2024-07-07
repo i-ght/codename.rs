bad8dc320bf7c6135acf552cbaa35aabb015b27c
i-ght <toph.ght@gmail.com>
2024-07-07 08:24:17 -0600

    subcrystaline_nematode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::fs::read_to_string;
-
+use std::{env, fs::read_to_string};
 use rand::Rng;
 
 fn lines(path: &str) -> std::io::Result<Vec<String>>{
-    let mut adjectives = Vec::with_capacity(8980);
+    let mut vals: Vec<String> = Vec::with_capacity(8980);
     
     for line in read_to_string(path)?.lines() {
-        adjectives.push(String::from(line));
+        vals.push(String::from(line));
     }
 
-    Ok(adjectives)
+    Ok(vals)
 }
 
 
@@ -19,15 +18,31 @@ fn main() -> std::io::Result<()> {
         lines("animals.txt")?
     );
 
+    let args: Vec<String> = env::args().collect();
+    
+    let count =
+        match args.get(1) {
+            Some(arg) =>
+                match arg.parse::<i32>() {
+                    Ok(count) if count <= 999 => count,
+                    _ => 1,
+                },
+            None => 1
+        };
+
+
     let mut rng = rand::thread_rng();
-    let (i, j) = (
-        rng.gen_range(0..adjectives.len()),
-        rng.gen_range(0..animals.len())
-    );
 
-    let (adj, animal) = (&adjectives[i], &animals[j]);
-
-    println!("{}_{}", adj, animal);
+    for _ in 0..count {
+        let (i, j) = (
+            rng.gen_range(0..adjectives.len()),
+            rng.gen_range(0..animals.len())
+        );
+    
+        let (adj, animal) = (&adjectives[i], &animals[j]);
+    
+        println!("{}_{}", adj, animal);
+    }
 
     Ok(())
 }
